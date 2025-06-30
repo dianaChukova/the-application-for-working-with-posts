71ce806deaf2266be4c26f0504e409ac148aa965
dianaChukova <diana_chukova@outlook.com>
2025-06-30 20:29:44 +0300

    Create detail post page + new ui component - Link

--- src/pages/posts/detail/index.jsx
+++ src/pages/posts/detail/index.jsx
@@ -1,2 +1,26 @@
+import { useParams } from "react-router-dom"
+import { INITIAL_POSTS } from "../index"
+import { useMemo } from "react"
+import { Typo } from "../../../components/Typo"
+import { Container } from "../../../components/Container/styles"
+import { Link } from "../../../components/Link"
+import * as SC from "./styles"
 
-export const DetailPostPage = () => <>Страница детального просмотра</>
+export const DetailPostPage = () => {
+    const { id } = useParams()
+    const currentPost = useMemo(() => INITIAL_POSTS.find((item) => item.id === Number(id)), [id])
+
+    if(!currentPost) {
+        return<>Пост не найден</>
+    }
+
+    return<Container>
+        <Typo>{currentPost.title}</Typo>
+        <SC.Image src={currentPost.image} alt={currentPost.title}/>
+        <SC.Text>{currentPost.text}</SC.Text>
+        <div style={{clear: "both"}}/>
+        <SC.LinkWrapper>
+            <Link to="/posts/">Назад</Link>
+        </SC.LinkWrapper>
+    </Container>
+}
--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -3,41 +3,48 @@ import { Posts } from "../../components/Posts"
 import { Typo } from "../../components/Typo"
 
 
-const INITIAL_POSTS =[
+export const INITIAL_POSTS =[
   {
     id: 1,
     title: 'Post 1',
-    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig'
+    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero praesentium excepturi facilis inventore repellendus accusamus quibusdam officia labore exercitationem quod qui obcaecati veniam, soluta error officiis dicta tempore eum."
   },
   {
     id: 2,
     title: 'Post 2',
-    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig'
+    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero praesentium excepturi facilis inventore repellendus accusamus quibusdam officia labore exercitationem quod qui obcaecati veniam, soluta error officiis dicta tempore eum."
   },
   {
     id: 3,
     title: 'Post 3',
-    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig'
+    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero praesentium excepturi facilis inventore repellendus accusamus quibusdam officia labore exercitationem quod qui obcaecati veniam, soluta error officiis dicta tempore eum."
   },
   {
     id: 4,
     title: 'Post 4',
-    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig'
+    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero praesentium excepturi facilis inventore repellendus accusamus quibusdam officia labore exercitationem quod qui obcaecati veniam, soluta error officiis dicta tempore eum."
   },
   {
     id: 5,
     title: 'Post 5',
-    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig'
+    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero praesentium excepturi facilis inventore repellendus accusamus quibusdam officia labore exercitationem quod qui obcaecati veniam, soluta error officiis dicta tempore eum."
   },
    {
     id: 6,
     title: 'Post 6',
-    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig'
+    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero praesentium excepturi facilis inventore repellendus accusamus quibusdam officia labore exercitationem quod qui obcaecati veniam, soluta error officiis dicta tempore eum."
   },
    {
     id: 7,
     title: 'Post 7',
-    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig'
+    image: 'https://avatars.mds.yandex.net/get-yapic/29310/RJsfWQOtUOe0TfYz45DAiQ4gd0-1/orig',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero praesentium excepturi facilis inventore repellendus accusamus quibusdam officia labore exercitationem quod qui obcaecati veniam, soluta error officiis dicta tempore eum."
   },
 ]
 
