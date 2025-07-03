f3b004ce11220eca699162cac0ad77e880011d98
dianaChukova <diana_chukova@outlook.com>
2025-07-03 23:27:03 +0300

    add-backend-for-posts

--- src/components/Posts/components/Post/index.jsx
+++ src/components/Posts/components/Post/index.jsx
@@ -1,8 +1,13 @@
 import { Link } from '../../../Link'
 import * as SC from './styles'
 
-export const Post = ({post}) => (<SC.Post>
-    <SC.Image src={post.image} alt={post.title}/>
-    <SC.Title>{post.title}</SC.Title>
-    <Link to={`/posts/${post.id}`}>Читать далее...</Link>
-</SC.Post>)
+export const Post = ({post}) => {
+    const image = post.image || "https://i.pinimg.com/736x/b2/1a/9f/b21a9ff51a2b511b3680603887147f01.jpg"
+    return (
+        <SC.Post>
+            <SC.Image src={image} alt={post.title}/>
+            <SC.Title>{post.title}</SC.Title>
+            <Link to={`/posts/${post.id}`}>Читать далее...</Link>
+        </SC.Post>
+    )
+}
