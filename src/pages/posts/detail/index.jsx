550f742fc4786f1e67c24a7ca1ef431f50ca9651
dianaChukova <diana_chukova@outlook.com>
2025-07-14 21:27:30 +0300

    add new files

--- src/pages/posts/detail/index.jsx
+++ src/pages/posts/detail/index.jsx
@@ -1,17 +1,26 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Typo } from "../../../components/Typo"
 import { Container } from "../../../components/Container/styles"
 import { Link } from "../../../components/Link"
 import * as SC from "./styles"
-import { getPostById, showPost } from "../../../redux/slices/postsSlice"
+import { getPostById, showPost, deletePost } from "../../../redux/slices/postsSlice"
 
 export const DetailPostPage = () => {
     const { id } = useParams()
     const {list} = useSelector((state) => state.posts.posts)
     const postForView = useSelector((state) => state.posts.postForView)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [postForDelete, setPostForDelete] = useState(null)
+
+    const onDeletePost = () => {
+        setPostForDelete(null)
+        dispatch(deletePost(postForDelete))
+
+        navigate('/posts')
+    }
 
     useEffect(() => {
         const intId = Number(id)
@@ -37,13 +46,25 @@ export const DetailPostPage = () => {
     const image = post.image || "https://i.pinimg.com/736x/b2/1a/9f/b21a9ff51a2b511b3680603887147f01.jpg"
 
     return<Container>
+        { postForDelete &&
+            <SC.ModalWrapper>
+                <SC.Modal>
+                    <SC.ModalText>Вы уверены, что хотите удалить этот пост (id-{postForDelete.id})?</SC.ModalText>
+                    <SC.ModalContent>
+                        <button onClick={() => setPostForDelete(null)}>Нет</button>
+                        <SC.DeleteButton onClick={onDeletePost}>Да</SC.DeleteButton>
+                    </SC.ModalContent>
+                </SC.Modal>
+            </SC.ModalWrapper>
+        }
         <Typo>{post.title}</Typo>
         <SC.Image src={image} alt={post.title}/>
         <SC.Text>{post.body}</SC.Text>
         <div style={{clear: "both"}}/>
         <SC.LinkWrapper>
             <Link to="/posts/">Назад</Link>
-            <Link to={`/posts/${post.id}/edit`}>Редактировать пост</Link>
+            {list && <Link to={`/posts/${post.id}/edit`}>Редактировать пост</Link>}
+            {list && <SC.DeleteButton onClick={() => setPostForDelete(post)}>Удалить</SC.DeleteButton>}
         </SC.LinkWrapper>
-    </Container>
+    </Container> 
 }
--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -59,7 +59,15 @@ export const postsSlice = createSlice({
                post: action.payload,
                loading: false
             }
-        } 
+        },
+        deletePost: (state, action) => {
+            state.posts.list = state.posts.list.filter((post) => post.id !== action.payload.id)
+            
+            state.postForView = {
+               post: null,
+               loading: false
+            }
+        },
     },
     extraReducers: (builder) => {
         builder.addCase(getPostById.pending,(state, action) => {
@@ -101,6 +109,6 @@ export const postsSlice = createSlice({
     }
 })
 
-export const { editPost, addPost, showPost} = postsSlice.actions
+export const { editPost, addPost, showPost, deletePost} = postsSlice.actions
 
 export default postsSlice.reducer
