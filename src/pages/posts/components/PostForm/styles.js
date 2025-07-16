83018af15b63db905d1e2f7812f978d8ec4d5e31
dianaChukova <diana_chukova@outlook.com>
2025-07-16 20:04:59 +0300

    Move part of componensts to ui and add regiatration

--- src/pages/posts/components/PostForm/styles.js
+++ src/pages/posts/components/PostForm/styles.js
@@ -1,29 +1,10 @@
 import styled from "styled-components";
 
-export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-    max-width: 250px;
-    margin: 30px auto;
-`
-
-export const Field = styled.div`
-   
-`
-
 export const Textarea = styled.textarea`
     resize: none;
     width: 100%;
     outline: none;
 `
-
-export const Input = styled.input`
-    width: 100%;
-    outline: none;
-    border: 1px solid black;
-`
-
 export const Button = styled.button`
     border: none;
     background: black;
--- src/pages/registration/index.jsx
+++ src/pages/registration/index.jsx
@@ -1,1 +1,94 @@
-export const RegistrationPage = () => <>Страница регистрации</>
+import { useId, useState } from "react";
+import { Container } from "../../components/ui/Container/styles";
+import { Typo } from "../../components/ui/Typo";
+import { Form } from "../../components/ui/Form";
+import { Field } from "../../components/ui/Field";
+import { Input } from "../../components/ui/Input";
+import { useNavigate } from "react-router-dom";
+
+export const RegistrationPage = () => {
+    const [formValues, setFormvalues] = useState({name: "" , surname: "", email: "", password: ""})
+    const navigate = useNavigate()
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        try{
+            const users = JSON.parse(localStorage.getItem("users"))
+            const userId = Date.now()
+            const newUser = { ...formValues, id: userId}
+
+            if (!users) {
+                localStorage.setItem("users", JSON.stringify([newUser]))
+                alert("Вы успешно зарегистрировались")
+                navigate('/auth')
+                return
+            }
+
+            if (users.find((user) => user.email === formValues.email)) {
+                alert("Пользователь с таким email уже существует")
+                return
+            }
+
+            users.push(newUser)
+
+            localStorage.setItem("users", JSON.stringify(users))
+
+            alert("Вы успешно зарегистрировались")
+
+            navigate("/auth")
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+     const onChange = (name, value) => {
+        setFormvalues({...formValues, [name]: value})
+    }
+
+    const disabled = !formValues.name || !formValues.surname || !formValues.email || !formValues.password
+
+    return (
+        <Container>
+            <Typo>Страница регистрации</Typo>
+            <Form onSubmit={onSubmit}>
+                <Field>
+                    <Input
+                        type="text" 
+                        name="name" 
+                        value={formValues.name}
+                        onChange={(e) => onChange(e.target.name, e.target.value)} 
+                        placeholder="Имя"
+                    />
+                </Field>
+                <Field>
+                    <Input
+                        type="text" 
+                        name="surname" 
+                        value={formValues.surname}
+                        onChange={(e) => onChange(e.target.name, e.target.value)} 
+                        placeholder="Фамилия"
+                    />
+                </Field>
+                <Field>
+                    <Input
+                        type="email" 
+                        name="email" 
+                        value={formValues.email}
+                        onChange={(e) => onChange(e.target.name, e.target.value)} 
+                        placeholder="Эл.почта"
+                    />
+                </Field>
+                <Field>
+                    <Input
+                        type="password" 
+                        name="password" 
+                        value={formValues.password}
+                        onChange={(e) => onChange(e.target.name, e.target.value)} 
+                        placeholder="Пароль"
+                    />
+                </Field>
+                <button type="submit" disabled= {disabled}>Зарегистрироваться</button>
+            </Form>
+        </Container>
+    )
+}
